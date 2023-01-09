4e62b7e74fd76dd4135d52ce621d7a6082baef59
Stelios Arakliotis <st.arakliotis@gmail.com>
2023-01-09 16:10:04 +0200

    Merge pull request #1 from lukaszwisniewski88/master
    Remove hook dependancies added bitwise operations

--- src/index.js
+++ src/index.js
@@ -1,77 +1,62 @@
-import React from "react";
-import PropTypes from "prop-types";
-import segments from "./lib/7segment";
+import React ,{useEffect,useRef,createRef} from 'react';
+import PropTypes from "prop-types"
+const SevenSegmentDisplay = ({number,style,ledColorOff,ledColorOn,ledBorder})=>{
+    const sevenSegmentObj={
+        "0":["a","b","c","d","e","f"],
+        "1":["b","c"],
+        "2":["a","b","d","e","g"],
+        "3":["a","b","c","d","g"],
+        "4":["b","c","f","g"],
+        "5":["a","c","d","f","g"],
+        "6":["a","c","d","e","f","g"],
+        "7":["a","b","c"],
+        "8":["a","b","c","d","e","f","g"],
+        "9":["a","b","c","d","f","g"],
+        "invalid":["g"]
+    }
+    const elementsRef = useRef(sevenSegmentObj[8].map(() => createRef()));
 
-/**
- *
- * @param character to display, if there will be more that one, display will fallback to E
- * @param style styles passed to SVG
- * @param ledColorOff color of the active segments
- * @param ledColorOn color of the incactive segments
- * @param ledBorder border color
- */
-const SevenSegmentDisplay = ({ character, style, ledColorOff, ledColorOn, ledBorder }) => {
-    const byteQuery = (int, i) => {
-        return int & (1 << i);
-    };
+    const refreshSegments =()=> {
+        elementsRef.current.forEach(polygon=>{
+            polygon.current.style.fill=ledColorOff
+        })
+    }
+
+    const calculateLeds =(num)=> {
+        sevenSegmentObj[8].filter(x => sevenSegmentObj[num].includes(x)).forEach(letter => {
+            elementsRef.current.forEach(polygon=>{
+                if(polygon.current.id === letter){
+                    polygon.current.style.fill=ledColorOn
+                }
+            })
+        });
+    }
+    useEffect(() => {
+        refreshSegments()
+        number >-1 && number < 10 ? calculateLeds(number): calculateLeds("invalid")
+    }, [number]);
+
     return (
         <svg style={style} xmlns="http://www.w3.org/2000/svg" width="192px" height="320px" viewBox="-1 -1 12 20">
-            <g
-                id="abcdefg"
-                style={{
-                    fillRule: "evenodd",
-                    stroke: `${ledBorder}`,
-                    strokeWidth: "0.25",
-                    strokeOpacity: "1",
-                    strokeLinecap: "butt",
-                    strokeLinejoin: "miter",
-                }}
-            >
-                <polygon
-                    id="a"
-                    points=" 1, 1  2, 0  8, 0  9, 1  8, 2  2, 2"
-                    fill={byteQuery(segments[character], 0) ? ledColorOn : ledColorOff}
-                />
-                <polygon
-                    id="b"
-                    points=" 9, 1 10, 2 10, 8  9, 9  8, 8  8, 2"
-                    fill={byteQuery(segments[character], 1) ? ledColorOn : ledColorOff}
-                />
-                <polygon
-                    id="c"
-                    points=" 9, 9 10,10 10,16  9,17  8,16  8,10"
-                    fill={byteQuery(segments[character], 2) ? ledColorOn : ledColorOff}
-                />
-                <polygon
-                    id="d"
-                    points=" 9,17  8,18  2,18  1,17  2,16  8,16"
-                    fill={byteQuery(segments[character], 3) ? ledColorOn : ledColorOff}
-                />
-                <polygon
-                    id="e"
-                    points=" 1,17  0,16  0,10  1, 9  2,10  2,16"
-                    fill={byteQuery(segments[character], 4) ? ledColorOn : ledColorOff}
-                />
-                <polygon
-                    id="f"
-                    points=" 1, 9  0, 8  0, 2  1, 1  2, 2  2, 8"
-                    fill={byteQuery(segments[character], 5) ? ledColorOn : ledColorOff}
-                />
-                <polygon
-                    id="g"
-                    points=" 1, 9  2, 8  8, 8  9, 9  8,10  2,10"
-                    fill={byteQuery(segments[character], 6) ? ledColorOn : ledColorOff}
-                />
+            <g id="abcdefg" style={{fillRule:'evenodd',stroke:`${ledBorder}`, strokeWidth:"0.25", strokeOpacity:"1", strokeLinecap:"butt", strokeLinejoin:"miter"}}>
+                <polygon ref={elementsRef.current[0]} id="a" points=" 1, 1  2, 0  8, 0  9, 1  8, 2  2, 2" fill={ledColorOff}/>
+                <polygon ref={elementsRef.current[1]} id="b" points=" 9, 1 10, 2 10, 8  9, 9  8, 8  8, 2" fill={ledColorOff}/>
+                <polygon ref={elementsRef.current[2]} id="c" points=" 9, 9 10,10 10,16  9,17  8,16  8,10" fill={ledColorOff}/>
+                <polygon ref={elementsRef.current[3]} id="d" points=" 9,17  8,18  2,18  1,17  2,16  8,16" fill={ledColorOff}/>
+                <polygon ref={elementsRef.current[4]} id="e" points=" 1,17  0,16  0,10  1, 9  2,10  2,16" fill={ledColorOff}/>
+                <polygon ref={elementsRef.current[5]} id="f" points=" 1, 9  0, 8  0, 2  1, 1  2, 2  2, 8" fill={ledColorOff}/>
+                <polygon ref={elementsRef.current[6]} id="g" points=" 1, 9  2, 8  8, 8  9, 9  8,10  2,10" fill={ledColorOff}/>
             </g>
         </svg>
-    );
-};
+    )
+}
+
 
 SevenSegmentDisplay.defaultProps = {
-    character: 0,
-    ledColorOff: "transparent",
-    ledColorOn: "red",
-    ledBorder: "black",
+    number: 0,
+    ledColorOff:"transparent",
+    ledColorOn:"red",
+    ledBorder:"black"
 };
 
 SevenSegmentDisplay.propTypes = {
