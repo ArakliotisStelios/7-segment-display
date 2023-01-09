cadfa59df7455821f7333b64d3f12a1cd5b065a4
Stelios Arakliotis <st.arakliotis@gmail.com>
2023-01-09 16:14:02 +0200

    Fresh package

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-
 var _react = _interopRequireWildcard(require("react"));
-
 var _propTypes = _interopRequireDefault(require("prop-types"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 var SevenSegmentDisplay = function SevenSegmentDisplay(_ref) {
   var number = _ref.number,
-      style = _ref.style,
-      ledColorOff = _ref.ledColorOff,
-      ledColorOn = _ref.ledColorOn,
-      ledBorder = _ref.ledBorder;
+    style = _ref.style,
+    ledColorOff = _ref.ledColorOff,
+    ledColorOn = _ref.ledColorOn,
+    ledBorder = _ref.ledBorder;
   var sevenSegmentObj = {
     "0": ["a", "b", "c", "d", "e", "f"],
     "1": ["b", "c"],
@@ -39,13 +32,11 @@ var SevenSegmentDisplay = function SevenSegmentDisplay(_ref) {
   var elementsRef = (0, _react.useRef)(sevenSegmentObj[8].map(function () {
     return /*#__PURE__*/(0, _react.createRef)();
   }));
-
   var refreshSegments = function refreshSegments() {
     elementsRef.current.forEach(function (polygon) {
       polygon.current.style.fill = ledColorOff;
     });
   };
-
   var calculateLeds = function calculateLeds(num) {
     sevenSegmentObj[8].filter(function (x) {
       return sevenSegmentObj[num].includes(x);
@@ -57,7 +48,6 @@ var SevenSegmentDisplay = function SevenSegmentDisplay(_ref) {
       });
     });
   };
-
   (0, _react.useEffect)(function () {
     refreshSegments();
     number > -1 && number < 10 ? calculateLeds(number) : calculateLeds("invalid");
@@ -115,9 +105,8 @@ var SevenSegmentDisplay = function SevenSegmentDisplay(_ref) {
     fill: ledColorOff
   })));
 };
-
 SevenSegmentDisplay.defaultProps = {
-  character: 0,
+  number: 0,
   ledColorOff: "transparent",
   ledColorOn: "red",
   ledBorder: "black"
